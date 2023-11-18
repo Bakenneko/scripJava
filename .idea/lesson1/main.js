cd8e827204072b0b15b08bf6c2139f410214fbfa
Andrew Kurylyk <andronniy7@gmail.com>
2023-11-18 18:03:40 +0200

    Initial commit

---
+++ .idea/lesson1/main.js
@@ -0,0 +1,28 @@
+
+const prompt = require('prompt-sync')();
+const script = require('./');
+const value = prompt("Enter name product").toLowerCase();
+let cost;
+switch (value) {
+    case "apple":
+        cost = 10;
+        break;
+    case "cherry":
+        cost = 50;
+        break;
+    case "pineapple":
+        cost = 40;
+        break;
+    case "watermelon":
+        cost = 110;
+        break;
+    case "balckberry":
+        cost = 10;
+        break;
+    case "melon":
+        cost = 150;
+        break;
+    default : alert(`Product ${value} is absent`);
+}
+
+alert(`${value} costs ${cost}`);
